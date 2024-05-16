8995746e3de1569bc129cc5bba7fc5e5923a4f72
Jan Dobes <dobesjan97@gmail.com>
2024-05-16 22:46:42 +0200

    remove unused

--- Eshop.Api/Eshop.Api/Controllers/EshopApiControllerBase.cs
+++ Eshop.Api/Eshop.Api/Controllers/EshopApiControllerBase.cs
@@ -7,51 +7,6 @@ namespace Eshop.Api.Controllers
 {
 	public class EshopApiControllerBase : Controller
 	{
-		protected IActionResult UpsertEntity<T>(T entity, IRepository<T> repository, bool validate=false) where T: Entity
-		{
-			if (validate)
-			{
-				var error = ValidateModel();
-				if (error != null)
-				{
-					return error;
-				}
-			}
-
-			try
-			{
-				if (entity != null)
-				{
-					if (entity.Id > 0)
-					{
-						if (!repository.IsStored(entity.Id))
-						{
-							return Json(new { success = false, message = $"{nameof(entity)} not found in db!" });
-						}
-
-						repository.Update(entity);
-						repository.Save();
-						return Json(new { success = true, message = $"{nameof(entity)} updated successfully" });
-					}
-					else
-					{
-						repository.Add(entity);
-						repository.Save();
-						return Json(new { success = true, message = $"{nameof(entity)} created successfully" });
-					}
-				}
-				else
-				{
-					return Json(new { success = false, message = $"{nameof(entity)} is null!" });
-				}
-
-			}
-			catch (Exception ex)
-			{
-				return Json(new { success = false, message = ex.Message });
-			}
-		}
-
 		protected IActionResult ValidateModel()
 		{
 			if (!ModelState.IsValid)
